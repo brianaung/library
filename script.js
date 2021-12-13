4caf0517c30a1d4d172e40bec021eedb582e65d8
Brian Aung <soekhantaung16@gmail.com>
2021-12-13 23:21:49 +1100

    implement function to manually add books

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let myLibrary = [];
 
-function Book() {
+const shelf = document.querySelector('.shelf');
+const addBtn = document.querySelector('.add-btn');
+
+addBtn.addEventListener('click', addBook);
+
+function Book(author, title, numPg) {
   // the constructor
+  this.author = author;
+  this.title = title;
+  this.numPg = numPg;
 }
 
-function addBook() {
-  // do stuff here
+function addBook(author, title, numPg) {
+  const book = new Book(author, title, numPg);
+  myLibrary.push(book);
 }
+
+function displayBook() {
+  // add the books inside the array to the shelf div
+  myLibrary.forEach(book => {
+    const b = document.createElement('div');
+    b.textContent = `${book.author}, ${book.title}, ${book.numPg}`;
+    shelf.appendChild(b);
+  });
+}
+
+addBook('brian', 'life sucks', 0);
+addBook('brian', 'everything sucks', 0);
+displayBook();
